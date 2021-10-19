89b8abf331ae14b3f438c393a435b9b071b79a23
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-10-18 22:46:16 -0300

    Delete desafio7/financys directory

--- desafio7/financys/src/main/java/com/example/financys/repository/EntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.financys.repository;
-
-import com.example.financys.entity.Entry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntryRepository extends JpaRepository<Entry, Long> {
-
-}
