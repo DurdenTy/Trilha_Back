58cefeeb8e4067d90fb05fcdf50c65486c8049da
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-10-21 12:39:23 -0300

    Delete desafio8/financys directory

--- desafio8/financys/src/main/java/com/example/financys/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.financys.repository;
-
-import com.example.financys.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
