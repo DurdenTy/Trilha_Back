2ab98e297e4b88765770e474c46885d5a60b8987
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-11-24 14:19:51 -0300

    Add files via upload

---
+++ Desafio13/aplicação/src/main/java/com/desafio10/Financys/Application/Controllers/CategoryController.java
@@ -0,0 +1,45 @@
+package com.desafio10.Financys.Application.Controllers;
+
+import com.desafio10.Financys.Domain.Entities.Category;
+import com.desafio10.Financys.Domain.Services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/categorias")
+    public Category create(@RequestBody Category category){
+        return categoryService.create(category);
+    }
+
+    @GetMapping("/categorias")
+    public List<Category> read(){
+        return categoryService.read();
+    }
+
+    @GetMapping("/categorias/{id}")
+    public Category readById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+
+    @PutMapping("/categorias/{id}")
+    public Category update(@RequestBody Category category, @PathVariable Long id){
+        return categoryService.update(category, id);
+    }
+
+    @DeleteMapping("/categorias/{id}")
+    public void delete(@PathVariable Long id){
+        categoryService.delete(id);
+    }
+}
